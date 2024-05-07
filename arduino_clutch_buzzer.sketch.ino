be09129ba164e870c8cfc7b4c8e1688f1c51d120
Spiros Evangelatos <spiros@augmenta.ag>
2024-05-07 15:56:13 +0300

    Implement unpark grace time and bonus to allow for parking

--- arduino_clutch_buzzer.sketch.ino
+++ arduino_clutch_buzzer.sketch.ino
@@ -1,9 +1,16 @@
 
 #define CLUTCH_PIN 3
 #define SPEAKER_PIN 8
-#define GRACE_INITIAL_MS 8000
-#define GRACE_SUBSEQUENT_MS 5000
+// In some period (unparking/parking) we allow up to 20 sec of clutch in
+#define GRACE_HIGH_MS 20000
+// In normal driving 5 sec should be plenty
+#define GRACE_LOW_MS 5000
 
+// We will be forgiving if you do not activate the buzzer for 2 minutes
+#define BONUS_PERIOD  120000
+
+// We will be forgiving for the first minute after the first clutch in
+#define UNPARK_PERIOD 60000
 
 // Possible states
 enum FsmState {
@@ -13,17 +20,25 @@ enum FsmState {
 };
 
 FsmState state;
+// Current grace time allowed without beeping
 unsigned grace_time_ms;
+// Time when the beeping will start if lutch not released
 unsigned long timeout_time;
+// Time of first clutch press
+unsigned long start_time;
+// Time of last activation
+unsigned long last_beep_time;
 
 void setup() {
   // Onboard LED mirrors the clutch input
   pinMode(LED_BUILTIN, OUTPUT);
   // We use pin 3 as input
   pinMode(CLUTCH_PIN, INPUT);
-  grace_time_ms = GRACE_INITIAL_MS;
+  grace_time_ms = GRACE_HIGH_MS;
   state = ST_OFF;
   timeout_time = 0;
+  start_time = 0;
+  last_beep_time = 0;
 }
 
 void buzz(bool on) {
@@ -47,19 +62,24 @@ bool isClutchIn() {
 void loop() {
   bool clutch_in = isClutchIn();
   FsmState new_state = state;
+  unsigned long now = millis();
 
   switch (state) {
     case ST_OFF:
       if (clutch_in) {
         new_state = ST_GRACE;
-        timeout_time = millis() + grace_time_ms;
+        timeout_time = now + grace_time_ms;
+        // Make a note of the start time
+        if (start_time == 0) {
+          start_time = now;
+        }
       }
       break;
 
     case ST_GRACE:
       if (clutch_in == false) {
         new_state = ST_OFF;
-      } else if (millis() >= timeout_time) {
+      } else if (now >= timeout_time) {
         new_state = ST_BUZZ;
       }
       break;
@@ -69,8 +89,16 @@ void loop() {
         new_state = ST_OFF;
       }
       // Next activation will be shorter
-      grace_time_ms = GRACE_SUBSEQUENT_MS;
+      grace_time_ms = GRACE_LOW_MS;
+      last_beep_time = now;
       break;
+  }
+
+  // If within the unpark period or you got the bonus, keep the grace time high
+  if (start_time == 0 ||
+      now < start_time + UNPARK_PERIOD ||
+      now > last_beep_time + BONUS_PERIOD) {
+    grace_time_ms = GRACE_HIGH_MS;
   }
 
   if (new_state != state) {
